130aace3b562d0be08b5a96976836ea5a4136b65
chris0610 <chrislow360@gmail.com>
2021-08-17 15:26:06 +0800

    add pagination

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ThumbUpIcon, ChatAlt2Icon } from "@heroicons/react/outline"
 
 import { getArticles } from "lib/helpers";
 
 export default function Home(props) {
-  const [articles, setArticles] = useState(props.articles);
+  const [pagesLoaded, setPagesLoaded] = useState(2);
+  const [currPage, setCurrPage] = useState(0);
+  const [paginationRange, setPaginationRange] = useState([currPage, currPage + 1, currPage + 2]);
+
+  const [allArticles, setAllArticles] = useState(props.articles);
+  const [currArticles, setCurrArticles] = useState(allArticles[0]);
+
+  const handlePrev = () => {
+    if (currPage > 0) setCurrPage(prev => prev - 1)
+  }
+
+  const handleNext = async () => {
+    const nextPage = currPage + 1;
+    if (nextPage > pagesLoaded) {
+      const nthPage = await getArticles(nextPage)
+
+      let newArticles = { ...allArticles }
+      newArticles[nextPage] = nthPage
+
+      setAllArticles(newArticles);
+      setPagesLoaded(prev => prev + 1);
+    }
+    setCurrPage(prev => prev + 1);
+  }
+
+  const getPaginationRange = () => {
+    if (currPage === 0) return [currPage, currPage + 1, currPage + 2]
+    else return [currPage - 1, currPage, currPage + 1]
+  }
+
+  useEffect(() => {
+    if (allArticles.hasOwnProperty(currPage)) {
+      setCurrArticles(allArticles[currPage])
+      setPaginationRange(getPaginationRange());
+    }
+  }, [currPage, allArticles])
 
   return (
     <div className="w-full m-auto py-10 prose lg:prose-lg">
@@ -14,7 +49,29 @@ export default function Home(props) {
         Latest News
       </h1>
 
-      {articles?.map(article => (
+      <div className="mb-4 w-full flex justify-between">
+        <button
+          disabled={currPage === 0}
+          onClick={handlePrev}
+          className={currPage === 0 ? 'text-gray-400 cursor-not-allowed' : ''}
+        >
+          Prev
+        </button>
+        <div className="flex space-x-4">
+          {paginationRange?.map(pageNo => (
+            <button
+              className={pageNo === currPage ? 'underline' : ''}
+            >
+              {pageNo + 1}
+            </button>
+          ))}
+        </div>
+        <button onClick={handleNext}>
+          Next
+        </button>
+      </div>
+
+      {currArticles?.map(article => (
         <Link key={article.id} href={article.id.toString()}>
           <div className="mb-8 flex space-x-10 cursor-pointer">
             <Image
@@ -52,11 +109,17 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const articles = await getArticles(0);
+  const firstPage = await getArticles(0);
+  const secondPage = await getArticles(1);
+  const thirdPage = await getArticles(2);
 
   return {
     props: {
-      articles
+      articles: [
+        firstPage,
+        secondPage,
+        thirdPage,
+      ]
     }
   }
 }
